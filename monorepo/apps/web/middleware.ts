fc48f10d61d9f771b1fc370172682a932da28d2e
ron2452 <matias.nahuel.pizzi@hotmail.com>
2026-01-15 21:51:55 -0300

    fix middleware

--- monorepo/apps/web/middleware.ts
+++ monorepo/apps/web/middleware.ts
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth"
 import { authConfig } from "./auth.config"
+import type { NextMiddleware } from "next/server"
 
-export default NextAuth(authConfig).auth
+const { auth } = NextAuth(authConfig)
+export default auth as NextMiddleware
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
