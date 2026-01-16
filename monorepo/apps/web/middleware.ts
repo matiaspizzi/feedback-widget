ac03379959e67fddeb766178324f39863b968cad
ron2452 <matias.nahuel.pizzi@hotmail.com>
2026-01-16 01:51:42 -0300

    fix cors

--- monorepo/apps/web/middleware.ts
+++ monorepo/apps/web/middleware.ts
@@ -3,8 +3,22 @@ import { authConfig } from "./auth.config"
 import type { NextMiddleware } from "next/server"
 
 const { auth } = NextAuth(authConfig)
-export default auth as NextMiddleware
+
+export default (async (req, ctx) => {
+  const res = await (auth as any)(req, ctx);
+
+  res.headers.append('Access-Control-Allow-Credentials', "true")
+  res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+  res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+  res.headers.append(
+    'Access-Control-Allow-Headers',
+    'x-api-key, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+
+  return res;
+}) as NextMiddleware;
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  // Allow execution on /api routes also, but keep excluding static files
+  matcher: ['/((?!_next/static|_next/image|.*\\.png$).*)'],
 }
