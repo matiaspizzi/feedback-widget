d333844f10ad57a2d7fc2f5944e1cfc95a9bf491
ron2452 <matias.nahuel.pizzi@hotmail.com>
2026-01-19 01:14:01 -0300

    improve linter

--- monorepo/packages/config-eslint/next.js
+++ monorepo/packages/config-eslint/next.js
@@ -9,15 +9,18 @@ module.exports = {
     "prettier",
     require.resolve("@vercel/style-guide/eslint/next"),
     "turbo",
+    "plugin:@typescript-eslint/recommended",
   ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["only-warn", "@typescript-eslint"],
   globals: {
     React: true,
     JSX: true,
   },
   env: {
     node: true,
+    browser: true,
   },
-  plugins: ["only-warn"],
   settings: {
     "import/resolver": {
       typescript: {
@@ -26,9 +29,23 @@ module.exports = {
     },
   },
   ignorePatterns: [
-    // Ignore dotfiles
     ".*.js",
     "node_modules/",
+    "dist/",
+    ".next/",
   ],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "caughtErrorsIgnorePattern": "^_",
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "warn",
+    "react/display-name": "off",
+  },
   overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
 };
