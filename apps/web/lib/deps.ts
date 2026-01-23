4cc2a2a5cede57bca5a7c835ed76e0bbff93d286
ron2452 <matias.nahuel.pizzi@hotmail.com>
2026-01-23 17:57:32 -0300

    singleton for deps (Module Caching)

--- apps/web/lib/deps.ts
+++ apps/web/lib/deps.ts
@@ -5,14 +5,23 @@ import {
 } from "@repositories";
 import { FeedbackService, UserService, ApiKeyService } from "@services";
 
+// Singleton instances
+const feedbackRepository = new FeedbackRepository();
+const apiKeyRepository = new ApiKeyRepository();
+const userRepository = new UserRepository();
+
+const feedbackService = new FeedbackService(feedbackRepository);
+const apiKeyService = new ApiKeyService(apiKeyRepository);
+const userService = new UserService(userRepository);
+
 export const getFeedbackDeps = () => ({
-  feedbackService: new FeedbackService(new FeedbackRepository()),
+  feedbackService,
 });
 
 export const getApiKeyDeps = () => ({
-  apiKeyService: new ApiKeyService(new ApiKeyRepository()),
+  apiKeyService,
 });
 
 export const getAuthDeps = () => ({
-  userService: new UserService(new UserRepository()),
+  userService,
 });
